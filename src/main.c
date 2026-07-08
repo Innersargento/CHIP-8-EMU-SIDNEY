d3ce139325a58ab8671eaac9c4157ea933c13953
sidaodomorro <danilaferrari77@gmail.com>
2026-07-08 15:01:45 -0300

    Iniciar implementação de uma janela com menu para carregar e renderizar

--- src/main.c
+++ src/main.c
@@ -1,21 +1,87 @@
-#include "cpu.h"
+#include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(){
-    chip_8 chip;
+#define WIDTH 480
+#define HEIGHT 360
 
-    chip_8_init(&chip);
+typedef struct {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+} chip_window;
 
-    for(int i = 0; i < 20; i++){
-        chip_8_loop(&chip);
-        for(int row = 0; row < HEIGHT; ++row ){
-            printf("\n");
-            for(int col = 0; col < WIDTH; ++col){
-                if(chip.screen[row][col] == true)
-                    printf("S");
-                else {
-                    printf(" ");
-                }
-            }
-        }
+typedef struct {
+    SDL_Rect rect;
+    int x;
+    int y;
+    int width;
+    int height;
+    char* text;
+    uint8_t ID;
+} menu_button;
+
+SDL_Color white = {
+    .r = 255,
+    .g = 255,
+    .b = 255,
+    .a = SDL_ALPHA_OPAQUE
+};
+
+SDL_Color black = {
+    .r = 0,
+    .g = 0,
+    .b = 0,
+    .a = SDL_ALPHA_OPAQUE
+};
+
+void init(chip_window* window){
+
+    if(!SDL_Init(SDL_INIT_VIDEO)){
+        fprintf(stderr, "SDL ERROR: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    if (!SDL_CreateWindowAndRenderer("CHIP-8-EMU-SIDNEY", WIDTH, HEIGHT, 0, &window->window, &window->renderer)){
+        fprintf(stderr, "ERROR: %s", SDL_GetError());
+        exit(EXIT_FAILURE);
     }
 }
+
+void clean(chip_window* window){
+
+    SDL_DestroyWindow(window->window);
+    SDL_DestroyRenderer(window->renderer);
+
+}
+
+void draw_canvas(SDL_Renderer* renderer, SDL_Color color){
+    SDL_SetRenderDrawColor(renderer, color.a, color.g, color.g, color.a);
+    SDL_RenderClear(renderer);
+}
+
+int main(){
+
+    chip_window window;
+    init(&window);
+
+    bool exit = false; 
+
+    while(!exit){
+        SDL_Event event;
+
+        while(SDL_PollEvent(&event)){
+            switch(event.type){
+                case SDL_EVENT_QUIT: exit = true; break;
+            }
+        }
+
+        SDL_RenderPresent(window.renderer);
+        draw_canvas(window.renderer, white);
+    }
+
+    clean(&window);
+}
+
+
+
