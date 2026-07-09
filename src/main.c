8cb451d7373da95b43278019e1f583b3555c8606
sidaodomorro <danilaferrari77@gmail.com>
2026-07-09 16:58:54 -0300

    Iniciar implementação de input

--- src/main.c
+++ src/main.c
@@ -25,6 +25,11 @@ SDL_Color black = {
     .a = SDL_ALPHA_OPAQUE
 };
 
+typedef struct {
+    menu_button buttons[BUTTONS];
+    TTF_Text* title;
+} menu;
+
 void screen_render(chip_8* chip, SDL_Renderer* renderer, SDL_Color color){
     for(int row = 0; row < 32; ++row ){
         for(int col = 0; col < 64; ++col){
@@ -37,24 +42,48 @@ void screen_render(chip_8* chip, SDL_Renderer* renderer, SDL_Color color){
     }
 }
 
+/*void input(chip_8* chip, Uint32 scan_code){
+    switch(scan_code){
+        case SDLK_1:
+        case SDLK_2:
+        case SDLK_3:
+        case SDLK_4:
+        case SDLK_Q:
+        case SDLK_W:
+        case SDLK_E:
+        case SDLK_R:
+        case SDLK_A:
+        case SDLK_S:
+        case SDLK_D:
+        case SDLK_F:
+        case SDLK_Z:
+        case SDLK_X:
+        case SDLK_C:
+        case SDLK_V:
+    }
+} */
+
 int main(){
 
     chip_window window;
     chip_8 chip;
+    menu main_menu;
 
     init(&window);
     chip_8_init(&chip);
+    
 
-    menu_button buttons[BUTTONS];
-
-    buttons[0] = (menu_button){"Load ROM", NULL, SCREEN_WIDTH/2, SCREEN_HEIGHT/5, BUTTON_WIDTH, BUTTON_HEIGHT, 0x01};
+    main_menu.buttons[0] = (menu_button){"Load ROM", NULL, SCREEN_WIDTH/2, SCREEN_HEIGHT/4, BUTTON_WIDTH, BUTTON_HEIGHT, 0x01};
+    main_menu.title = TTF_CreateText(window.engine, window.font, "CHIP-8-EMU", 0);
+    int text_w, text_h;
+    TTF_GetTextSize(main_menu.title, &text_w, &text_h);
 
     rom_request req;
     req.path[0] = '\0';
     SDL_SetAtomicInt(&req.ready, 0);
     bool rom_loaded = false;
 
-    init_buttons(&window, buttons, white, BUTTONS);
+    init_buttons(&window, main_menu.buttons, white, BUTTONS);
 
     bool exit = false; 
 
@@ -64,11 +93,11 @@ int main(){
         while(SDL_PollEvent(&event)){
             switch(event.type){
                 case SDL_EVENT_QUIT: exit = true; break;
-                case SDL_EVENT_MOUSE_BUTTON_DOWN: uint8_t ID = button_pressed(buttons, event.motion.x, event.motion.y, BUTTONS);
+                case SDL_EVENT_MOUSE_BUTTON_DOWN: uint8_t ID = button_pressed(main_menu.buttons, event.motion.x, event.motion.y, BUTTONS);
                 switch(ID){
                     case 0x01: open_rom_dialog(&window, &req); break;
                 };
-                break;
+                break;  
             }
         }
         if (SDL_GetAtomicInt(&req.ready)) {
@@ -79,7 +108,8 @@ int main(){
         }
         draw_canvas(window.renderer, black);
         if(!rom_loaded){
-            render_buttons(&window, buttons, white, black, white, BUTTONS);
+            render_buttons(&window, main_menu.buttons, white, black, white, BUTTONS);
+            TTF_DrawRendererText(main_menu.title, SCREEN_WIDTH/2 - text_w/2, 10);
         } else {
             chip_8_loop(&chip);
             screen_render(&chip, window.renderer, white);
@@ -87,9 +117,8 @@ int main(){
         SDL_RenderPresent(window.renderer);
     }
 
-    destroy_buttons(buttons, BUTTONS);
+    destroy_buttons(main_menu.buttons, BUTTONS);
     clean(&window);
 }
-
 
 
